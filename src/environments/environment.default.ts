cbfd9eb8ad1680d447ecfb2ad18d7d2e86505316
Zayd EL IMRANI <elimrani.z@gmail.com>
2023-09-16 15:30:45 +0100

    Default Env

---
+++ src/environments/environment.default.ts
@@ -0,0 +1,12 @@
+export const defaultEnvironment = {
+    firebase: {
+        apiKey: "AIzaSyAHdUFfObS-CW40Y-Nyoz988VFES3FS7hQ",
+        authDomain: "dev-guide-8cd95.firebaseapp.com",
+        projectId: "dev-guide-8cd95",
+        storageBucket: "dev-guide-8cd95.appspot.com",
+        messagingSenderId: "966107072636",
+        appId: "1:966107072636:web:1a99ac39605f48c36521f9",
+        measurementId: "G-9PGERS55X8"
+    },    
+    production: false
+};
